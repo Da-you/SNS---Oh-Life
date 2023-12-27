1498cf19ba7692e521edd365323b3e376feb2e79
JoHyunwoo <jhw3874@gmail.com>
2023-12-27 20:03:11 +0900

    init : project initializr

---
+++ src/main/java/project/ohlife/OhLifeApplication.java
@@ -0,0 +1,13 @@
+package project.ohlife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OhLifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/ohlife/OhLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package project.ohlife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
